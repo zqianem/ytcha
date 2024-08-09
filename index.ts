15ebef5774bad3d6c6ceeb499b6d3b777005b514
Em Zhan <zqianem@gmail.com>
2024-08-09 13:21:36 -0500

    Print better error messages

--- index.ts
+++ index.ts
@@ -3,13 +3,20 @@ import puppeteer from 'puppeteer'
 import which from 'which'
 
 program.argument('<channel>', 'YouTube channel name, e.g. @EthosLab')
+program.showHelpAfterError();
+program.allowExcessArguments(false)
 program.parse()
 const channel = program.args[0]
 
-const browser = await puppeteer.launch({
-  browser: 'firefox',
-  executablePath: await which('firefox'),
-})
+let executablePath: string
+try {
+  executablePath = await which('firefox')
+} catch {
+  console.error('Firefox is required but not installed')
+  process.exit(1)
+}
+
+const browser = await puppeteer.launch({ browser: 'firefox', executablePath })
 const page = await browser.newPage()
 
 const url = `https://www.youtube.com/${channel}/videos`
