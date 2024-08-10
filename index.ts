0f6eed46a2b9455253b0ab8da3e7311a3e249778
Em Zhan <zqianem@gmail.com>
2024-08-10 09:37:40 -0500

    Refactor to make it easier to add new browsers

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { program } from 'commander'
-import puppeteer from 'puppeteer'
+import puppeteer, { SupportedBrowser } from 'puppeteer'
 import which from 'which'
 
 program
@@ -12,27 +12,33 @@ program
 const channel = program.args[0]
 const options = program.opts()
 
-const browsers = [
-  'brave',
-  'chromium',
-  'chrome',
-  'firefox'
-]
+const browsers: Record<string, SupportedBrowser> = {
+  brave: 'chrome',
+  chromium: 'chrome',
+  chrome: 'chrome',
+  firefox: 'firefox',
+}
 
-const executablePath = await browsers.reduce(async (prev, curr) => {
-  return (await prev) ? prev : which(curr).catch(() => undefined)
-}, Promise.resolve(undefined) as Promise<string | undefined>)
+let executablePath: string | undefined = undefined
+let browser: SupportedBrowser | undefined = undefined
+
+for (const [key, value] of Object.entries(browsers)) {
+  try {
+    executablePath = await which(key)
+    browser = value
+    break
+  } catch {
+    continue
+  }
+}
 
 if (!executablePath) {
   console.error('No compatible browser found for web scraping')
   process.exit(1)
 }
 
-const browser = await puppeteer.launch({
-  browser: executablePath.includes('firefox') ? 'firefox' : 'chrome',
-  executablePath
-})
-const page = await browser.newPage()
+const puppet = await puppeteer.launch({ executablePath, browser })
+const page = await puppet.newPage()
 
 const url = `https://www.youtube.com/${channel}/videos`
 await page.goto(url)
